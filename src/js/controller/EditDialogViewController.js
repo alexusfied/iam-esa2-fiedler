68f9e41a8c7a6a771d75b4bbbc7dcdf0fa09a602
Alex <alexf137@posteo.de>
2025-06-25 22:06:02 +0200

    Origin private file system used for saving media item img files during app use

--- src/js/controller/EditDialogViewController.js
+++ src/js/controller/EditDialogViewController.js
@@ -1,5 +1,6 @@
 import {GenericDialogTemplateViewController} from "vfh-iam-mwf-base";
 import ExifReader from "exifreader";
+import {LocalFileSystemReferenceHandler} from "../model/LocalFileSystemReferenceHandler";
 
 export default class EditDialogViewController extends GenericDialogTemplateViewController {
 // instance attributes set by mwf after instantiation
@@ -11,44 +12,40 @@ export default class EditDialogViewController extends GenericDialogTemplateViewC
     console.log("EditDialogViewController.onresume(): ", this.args, this.root);
     await super.onresume();
 
-    var mediaItem = this.args.item;
+    const mediaItem = this.args.item;
+    const fsHandler = await LocalFileSystemReferenceHandler.getInstance();
 
     this.viewProxy = this.bindElement("mediaItemDialog", {
       item: mediaItem,
     }, this.root).viewProxy;
 
-    this.viewProxy.bindAction("fileSelected", ((event) => {
+    this.viewProxy.bindAction("fileSelected", (async (event) => {
       event.original.preventDefault();
       if (event.original.target.files[0]) {
-        const fileReader = new FileReader();
         const imageFile = event.original.target.files[0];
-        fileReader.readAsDataURL(imageFile);
-        fileReader.onload = async () => {
-          mediaItem.src = fileReader.result;
+        const localReference = await fsHandler.createLocalFileSystemReference(imageFile);
 
-          if (!mediaItem.title) {
-            mediaItem.title = imageFile.name;
-          }
+        mediaItem.src = await fsHandler.resolveLocalFileSystemReference(localReference);
+        if (!mediaItem.title) {
+          mediaItem.title = localReference.replace("opfs://", "");
+        }
 
-          const tags = await ExifReader.load(imageFile, {expanded: true});
+        const tags = await ExifReader.load(imageFile, {expanded: true});
+        if (tags.exif && tags.exif.GPSLatitude && tags.exif.GPSLongitude) {
+          mediaItem.latlng = {
+            lat: tags.exif.GPSLatitude.description,
+            lng: tags.exif.GPSLongitude.description
+          };
+        } else {
+          // Default location info, if the image doesn't contain the info in the metadata
+          mediaItem.latlng = {
+            lat: 52.416,
+            lng: 12.55
+          };
+        }
 
-          if (tags.exif && tags.exif.GPSLatitude && tags.exif.GPSLongitude) {
-            mediaItem.latlng = {
-              lat: tags.exif.GPSLatitude.description,
-              lng: tags.exif.GPSLongitude.description
-            };
-          } else {
-            // Default location info, if the image doesn't contain the info in the metadata
-            mediaItem.latlng = {
-              lat: 52.416,
-              lng: 12.55
-            };
-          }
-
-          this.viewProxy.update({item: mediaItem});
-        };
+        this.viewProxy.update({item: mediaItem});
       }
     }));
-// ADD YOUR OWN IMPLEMENTATION HERE
   }
 }
